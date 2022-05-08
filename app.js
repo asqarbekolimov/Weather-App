29778c05659da781cd02fb5d7d8b3d22cf2e99cb
Asqarbek Olimov <88034288+AsqarbekOlimov@users.noreply.github.com>
2022-05-08 23:21:49 +0500

    Weather App

---
+++ app.js
@@ -0,0 +1,55 @@
+const api = {
+  key: '149649e4bb2c685ed08f1d1cc5cbbcab',
+  baseUrl: 'https://api.openweathermap.org/data/2.5/',
+};
+
+const searchBox = document.querySelector('.search-box');
+
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(e) {
+  if (e.keyCode === 13) {
+    getResults(searchBox.value)
+    console.log(searchBox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.baseUrl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city')
+  city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerHTML = dateBuilder(now);
+
+  let temp =  document.querySelector('.temp')
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span><sup>0</sup>c</span>`
+
+  let weatherEl = document.querySelector('.weather');
+  weatherEl.innerHTML = weather.weather[0].main, weather.weather[0].icon;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerHTML = `${Math.round(weather.main.temp_min)}<span><sup>0</sup>c</span>/${Math.round(weather.main.temp_max)}<span><sup>0</sup>c</span>`
+}
+
+function dateBuilder(s) {
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', ]
+
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ];
+
+  let day = days[s.getDay()];
+  let date = s.getDate();
+  let month = months[s.getMonth()];
+  let year = s.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+}
